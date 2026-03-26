8f95db35f3bba8f75cdb5133ba8cfb2760053666
raihana10 <raihanamohito@gmail.com>
2026-03-26 23:30:49 +0100

    feat: add AdminController for managing orders and clients, and a new login FXML.

--- src/main/java/ui/AdminController.java
+++ src/main/java/ui/AdminController.java
@@ -11,6 +11,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.util.Callback;
 import shared.Reponse;
 import shared.Requete;
 import shared.RequestType;
